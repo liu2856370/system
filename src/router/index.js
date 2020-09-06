ffe4ae6c4f1206c8b1af7739ea2767be8e41a1f6
sunwenjunnet <sunwenjunnet@sina.com>
2020-09-06 22:09:12 +0800

    提交登录页路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,29 +4,39 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [{
-    path: '/',
-    name: 'home',
-    component: () => import( /* webpackChunkName: "首页" */ '../modules/home/Index.vue')
-  },
-  {
-    path: '/info',
-    name: 'info',
-    component: () => import( /* webpackChunkName: "信息查询首页" */ '../modules/info/Index.vue')
-  },
-  {
-    path: '/platform',
-    name: 'platform',
-    component: () => import(/* webpackChunkName: "平台首页" */ '../modules/platform/Index.vue')
-  },
-  {
-    path: '/process-info',
-    name: 'process-info',
-    component: () => import(/* webpackChunkName: "过程信息" */ '../modules/info/ProcessInfo.vue')
-  }
+        path: '/',
+        name: 'home',
+        component: () =>
+            import ( /* webpackChunkName: "首页" */ '../modules/home/Index.vue')
+    },
+    {
+        path: '/info',
+        name: 'info',
+        component: () =>
+            import ( /* webpackChunkName: "信息查询首页" */ '../modules/info/Index.vue')
+    },
+    {
+        path: '/platform',
+        name: 'platform',
+        component: () =>
+            import ( /* webpackChunkName: "平台首页" */ '../modules/platform/Index.vue')
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () =>
+            import ( /* webpackChunkName: "平台首页" */ '../modules/platform/Login.vue')
+    },
+    {
+        path: '/process-info',
+        name: 'process-info',
+        component: () =>
+            import ( /* webpackChunkName: "过程信息" */ '../modules/info/ProcessInfo.vue')
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
