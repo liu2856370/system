21b30fca69cb1823ce770ccb26fd77d6f2ad4288
刘宗权 <>
2020-09-07 21:03:26 +0800

    修改列表

--- src/modules/platform/common/platformList.vue
+++ src/modules/platform/common/platformList.vue
@@ -2,9 +2,16 @@
   <div class="list">
     <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
       <van-cell-group v-for="(item, index) in list" :key="index" class="mt10">
+        <div>
           <slot name="fixed" :slotProps="item"></slot>
-          <slot v-if="unfold" name="variable" :slotProps="item"></slot>
-          <div :class="unfold?'unfold':'noUnfold'" @click="toggleShow">{{arrowTit}}</div>
+        </div>
+        <div v-show="item.unfold">
+          <slot name="variable" :slotProps="item"></slot>
+        </div>
+        <div class="arrow-outer" @click="toggleShow(index)">
+            <van-icon v-show="item.unfold" name="arrow-up" />
+            <van-icon v-show="!item.unfold"  name="arrow-down" />
+        </div>
       </van-cell-group>
     </van-list>
   </div>
@@ -22,41 +29,35 @@ export default {
   data() {
     return {
       loading: false,
-      finished: false,
-      unfold:false,
-      arrowTit:"展开"
-    }
+      finished: false
+    };
   },
-  methods:{
-      onLoad(){},
-      toggleShow(){
-          this.unfold = !this.unfold;
-          if(this.unfold){
-              this.arrowTit = "收起";
-          }else{
-               this.arrowTit = "展开";
-          }
-      }
-  }
+  methods: {
+    onLoad() {},
+    toggleShow(index) {
+        console.log(this.list[index].unfold)
+        this.$set(this.list[index],"unfold",!this.list[index].unfold);
+    },
+  },
 };
 </script>
 
 <style lang="less" scoped>
-.hide{
-    display: none;
+.arrow-outer{
+    text-align: center;
+    font-size:0.22rem;
+    height: 0.3rem;
+    line-height:0.3rem;
+    border-top: 1px solid #ebedf0;
 }
-.show{
-    display: block;
+.org-info {
+  padding: 0.1rem 0.15rem;
+  color: #7b7b7b;
+  .org-name {
+    font-size: 0.16rem;
+  }
+  .org-tags {
+    text-align: right;
+  }
 }
-.org-info{
-    padding: 0.1rem 0.15rem;
-    color: #7b7b7b;
-    .org-name{
-        font-size:0.16rem;
-    }
-    .org-tags{
-        text-align: right;
-    }
-}
-
 </style>
