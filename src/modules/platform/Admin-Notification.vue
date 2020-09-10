35029b376990f0747da573620aef7d64676b6e27
unknown <737579732@qq.com>
2020-09-10 08:50:35 +0800

    提交新页面

---
+++ src/modules/platform/Admin-Notification.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <PHeader :showArrow="true" @leftClick="onClickLeft">
+            <template #default>受理通知书</template>
+        </PHeader>
+        <van-image width="1.35rem" height="1.5rem" fit="现场审查技..." src="https://img.yzcdn.cn/vant/cat.jpeg">
+            <template #default>
+                <div class="mt10" ></div>
+                现场审查技...
+            </template>
+        </van-image>
+        <van-image width="1.35rem" height="1.5rem" fit="现场审查技..." src="https://img.yzcdn.cn/vant/cat.jpeg">
+            <template #default>
+                <div class="mt10" ></div>
+                现场审查技...
+            </template>
+        </van-image>
+        <van-image width="1.35rem" height="1.5rem" fit="现场审查技..." src="https://img.yzcdn.cn/vant/cat.jpeg">
+            <template #default>
+                <div class="mt10" ></div>
+                现场审查技...
+            </template>
+        </van-image>
+        
+    </div>
+</template>
+<script>
+import PHeader from "../../components/PHeader.vue";
+export default {
+    data(){
+        return{};
+    },
+    components: {
+        PHeader,
+    },
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/modules/platform/Admin-Returnable.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <PHeader :showArrow="true" @leftClick="onClickLeft">
+            <template #default>材料退回</template>
+        </PHeader>
+        <template style="margin-top: 0.1rem;">
+            <van-cell title="退回时间" value="2020-05-26"/>
+            <van-cell title="备注">
+              <template #right-icon>
+                <van-tag plain round type="primary" @click="queryBasisList">查看</van-tag>
+              </template>
+            </van-cell>
+        </template>
+    </div>
+</template>
+<script>
+import PHeader from "../../components/PHeader.vue";
+export default {
+    name: "adminReturnable",
+    data(){
+        return {};
+    },
+    components: {
+    PHeader,
+  },
+}
+</script>
