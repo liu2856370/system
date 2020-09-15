0e091d1ae3ce661badc2fd809264a4c9ad37fafc
unknown <737579732@qq.com>
2020-09-15 23:30:20 +0800

    行政审批页面修改

--- src/modules/platform/Admin-Observer.vue
+++ src/modules/platform/Admin-Observer.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-      <PHeader :showArrow="true" @leftClick="onClickLeft">
+      <PHeader :showArrow="true">
             <template #default>观察员报告</template>
       </PHeader>  
         <template>
-        <van-cell title="观察员" value="李冬冬" />
-        <van-cell title="性别" value="男" />
-        <van-cell title="手机" value="" />
-        <van-cell title="电话" value="" />
-        <van-cell title="邮箱" value="" />
-        <van-cell title="观察时间" value="" />
-        <van-cell title="上传附件" value="" />
-        <van-cell title="观察员意见" value="" />
-        <van-cell title="观察信息上报时间" value="" />
+          <div style="margin-top: 0.1rem;">
+            <van-cell title="观察员" value="李冬冬" />
+            <van-cell title="性别" value="男" />
+            <van-cell title="手机" value="" />
+            <van-cell title="电话" value="" />
+            <van-cell title="邮箱" value="" />
+            <van-cell title="观察时间" value="" />
+            <van-cell title="上传附件" value="" />
+            <van-cell title="观察员意见" value="" />
+            <van-cell title="观察信息上报时间" value="" />
+          </div>
         </template>
   </div>
 </template>
@@ -21,10 +23,14 @@ import PHeader from "../../components/PHeader.vue";
 export default {
     name: "administrationObserver",
     data(){
-        return{};
+        return{
+          companyData:{}
+        };
     },
     components: {
     PHeader,
   },
+  methods:{
+  }
 };
 </script>
