1c62439a69faa85c1f89336d4ff767418b5c587b
sunwenjunnet <sunwenjunnet@sina.com>
2020-09-16 00:34:00 +0800

    提交demo

--- src/modules/info/Demo.vue
+++ src/modules/info/Demo.vue
@@ -5,8 +5,18 @@
       value=""
       value-class="filterLabel"
     >
-    <template #default>筛选<i class="icon-filter"></i></template>
+      <template #default>
+        <div @click="showPopup">筛选<i class="icon-filter"></i></div>
+      </template>
     </van-cell>
+
+    <van-popup
+      v-model="show"
+      get-container="body"
+      position="right"
+      closeable
+      :style="{ width: '80%',height:'100%'}"
+    >内容</van-popup>
 
     <ul class="licenseCard">
       <li class="upComing">
@@ -22,15 +32,21 @@
 </template>
 <script>
 import Vue from "vue";
-import { Cell, CellGroup } from "vant";
+import { Cell, Popup } from "vant";
 
 Vue.use(Cell);
-Vue.use(CellGroup);
+Vue.use(Popup);
 export default {
   data() {
     return {
       processTotal: "共21条",
+      show: false,
     };
+  },
+  methods: {
+    showPopup() {
+      this.show = true;
+    },
   },
 };
 </script>
