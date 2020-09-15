2d01ffa15c5b86dbc5d3333b3cf829d6509fc369
sunwenjunnet <sunwenjunnet@sina.com>
2020-09-16 00:00:40 +0800

    添加demo

---
+++ src/modules/info/Demo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <van-cell
+      :title="processTotal"
+      value=""
+      value-class="filterLabel"
+    >
+    <template #default>筛选<i class="icon-filter"></i></template>
+    </van-cell>
+
+    <ul class="licenseCard">
+      <li class="upComing">
+        <p>许可代办</p>
+        <label>99</label>
+      </li>
+      <li class="done">
+        <p>许可已办</p>
+        <label>210</label>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+import { Cell, CellGroup } from "vant";
+
+Vue.use(Cell);
+Vue.use(CellGroup);
+export default {
+  data() {
+    return {
+      processTotal: "共21条",
+    };
+  },
+};
+</script>
