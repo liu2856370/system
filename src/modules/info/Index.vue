a67c21df9c27c5ac07b7030963785630ff1c194d
24kcsxpp <2365129464@qq.com>
2020-09-16 13:10:16 +0800

    修改

--- src/modules/info/Index.vue
+++ src/modules/info/Index.vue
@@ -4,25 +4,9 @@
     <van-search v-model="value" shape="round" placeholder="请输入单位名称" />
     <van-tabs v-model="active" sticky>
       <van-tab title="过程信息">
-        <van-cell :title="processTotal" value="筛选" />
-        <platform-list :list="processList">
-          <template #fixed="{slotProps}">
-            <van-row class="org-info">
-              <van-col span="14" class="org-name" @click="goVerificationInfo(slotProps)">{{slotProps.orgname}}</van-col>
-              <van-col span="10" class="org-tags">
-                <van-tag plain round type="primary" size="large" class="mr10">{{slotProps.flag|dictFormatter("businessType")}}</van-tag>
-                <van-tag plain round type="primary" size="large">{{slotProps.applydescription}}</van-tag>
-              </van-col>
-            </van-row>
-            <van-cell title="产品类别" :value="slotProps.prodtype" />
-            <van-cell title="审批事项" :value="slotProps.itemName" />
-          </template>
-          <template #variable="{slotProps}">
-            <van-cell title="所在地区" :value="slotProps.region" />
-            <van-cell title="营业执照注册号" :value="slotProps.busliceno" />
-            <van-cell title="组织机构代码" :value="slotProps.organno" />
-          </template>
-        </platform-list>
+        <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad" class="mt10" >
+          <van-cell v-for="item in list" :key="item.caption" :title="item.caption" is-link @click="goInfo(item)" />
+        </van-list>
       </van-tab>
       <van-tab title="证书信息">
         <van-cell :title="certificateTotal" value="" />
@@ -64,6 +48,7 @@ export default {
       finished: true,
       loading: false,
       itemData: "",
+      list: [],
       processList: [],
       certificateList:{},
       certificateCompanyList:[]
@@ -71,6 +56,10 @@ export default {
   },
   methods:{
     onLoad(){},
+    goInfo(item){
+      client.saveStorage("approvalInfo", item);
+      this.$router.push("/process-info");
+    },
     saveData(item){
       this.itemData = item;
     },
@@ -83,28 +72,13 @@ export default {
       this.$router.push("/qualifications");
     },
     goVerificationInfo(itemData){
-      client.saveStorage("companyProcessInfo", itemData);
-      if(itemData.applydescription === "发证"){
-        this.$router.push("/certification-check");
-      }
-      else if(itemData.applydescription === "变更"){
-        this.$router.push("/change-check");
-      }
-      else if(itemData.applydescription === "换证"){
-        this.$router.push("/replace-check");
-      }
-
-      // //受理中计划中
-      // if(20000<=itemData.state&&itemData.state<50000){
-      //   this.$router.push("/replace-check");
-      // }
-      // //审查中、许可中、已发证、不予许可
-      // else if((50000<=itemData.state&&itemData.state<=80000) || (parseInt(itemData.state) === 10700)){
-      //   this.$router.push("/certification-check");
-      // }
     }
   },
   created(){
+    client.rpc("/dic/getSpxx").then(res=>{
+        this.list = res;
+    });
+
       client.rpc("/xxgs/findGcxx",{"itemId": client.loadStorage("approvalInfo").code}).then(res=>{
         this.processTotal = "共" + res.list.length + "条";
         this.processList = res.list;
--- src/modules/info/ProcessInfo.vue
+++ src/modules/info/ProcessInfo.vue
@@ -1,29 +1,109 @@
 <template>
-  <div>
-      <van-tabs v-model="activeName">
-        <van-tab title="过程信息" name="process">过程信息</van-tab>
-        <van-tab title="证书信息" name="certificate">证书信息</van-tab>
-      </van-tabs>
+  <div class="index">
+    <PHeader :showArrow="true">过程信息</PHeader>
+    <van-search v-model="value" shape="round" placeholder="请输入单位名称" />
+        <van-cell :title="processTotal" value="筛选" />
+        <platform-list :list="processList">
+          <template #fixed="{slotProps}">
+            <van-row class="org-info">
+              <van-col span="14" class="org-name" @click="goVerificationInfo(slotProps)">{{slotProps.orgname}}</van-col>
+              <van-col span="10" class="org-tags">
+                <van-tag plain round type="primary" size="large" class="mr10">{{slotProps.flag|dictFormatter("businessType")}}</van-tag>
+                <van-tag plain round type="primary" size="large">{{slotProps.applydescription}}</van-tag>
+              </van-col>
+            </van-row>
+            <van-cell title="产品类别" :value="slotProps.prodtype" />
+            <van-cell title="审批事项" :value="slotProps.itemName" />
+          </template>
+          <template #variable="{slotProps}">
+            <van-cell title="所在地区" :value="slotProps.region" />
+            <van-cell title="营业执照注册号" :value="slotProps.busliceno" />
+            <van-cell title="组织机构代码" :value="slotProps.organno" />
+          </template>
+        </platform-list>
   </div>
 </template>
 
 <script>
-    import { Tab, Tabs } from 'vant';
+import PHeader from "../../components/PHeader.vue";
+import platformList from "../common/platformList";
+export default {
+  name: "index",
+  components: {
+    platformList,
+    PHeader
+  },
+  data() {
+    return {
+      radio: 0,
+      processTotal: "",
+      certificateTotal: "",
+      active: 0,
+      value: "",
+      finished: true,
+      loading: false,
+      itemData: "",
+      processList: [],
+      certificateList:{},
+      certificateCompanyList:[]
+    };
+  },
+  methods:{
+    onLoad(){},
+    goVerificationInfo(itemData){
+      client.saveStorage("companyProcessInfo", itemData);
+      if(itemData.applydescription === "发证"){
+        this.$router.push("/certification-check");
+      }
+      else if(itemData.applydescription === "变更"){
+        this.$router.push("/change-check");
+      }
+      else if(itemData.applydescription === "换证"){
+        this.$router.push("/replace-check");
+      }
 
-    export default {
-        name: 'process-info',
-        components:{
-            "van-tab":Tab,
-            "van-tabs":Tabs
-        },
-        data() {
-            return {
-                activeName: "process"
-            }
-        }
+      // //受理中计划中
+      // if(20000<=itemData.state&&itemData.state<50000){
+      //   this.$router.push("/replace-check");
+      // }
+      // //审查中、许可中、已发证、不予许可
+      // else if((50000<=itemData.state&&itemData.state<=80000) || (parseInt(itemData.state) === 10700)){
+      //   this.$router.push("/certification-check");
+      // }
     }
+  },
+  created(){
+      client.rpc("/xxgs/findGcxx",{"itemId": client.loadStorage("approvalInfo").code}).then(res=>{
+        this.processTotal = "共" + res.list.length + "条";
+        this.processList = res.list;
+      });
+  }
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+.van-radio,
+.van-button,
+.van-notice-bar{
+  padding: 10px 16px;
+}
 
+.van-radio{
+  padding: 10px 16px;
+  overflow: hidden;
+  font-size: 14px;
+  line-height: 24px;
+}
+
+.org-info {
+  padding: 0.1rem 0.15rem;
+  color: #7b7b7b;
+  .org-name {
+    font-size: 0.16rem;
+    color: #1989fa;
+  }
+  .org-tags {
+    text-align: right;
+  }
+}
 </style>
