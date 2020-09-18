64330cd3c5afe2bbf2fac6f11836b64e62264fe7
unknown <737579732@qq.com>
2020-09-18 22:32:16 +0800

    样式优化

--- src/modules/info/CertificationCheck.vue
+++ src/modules/info/CertificationCheck.vue
@@ -1,45 +1,51 @@
 <template>
   <div class="index">
     <PHeader :showArrow="true">核查信息</PHeader>
-    <van-tabs v-model="active" sticky>
+    <van-tabs v-model="active" :border="false" :ellipsis="false" swipe-threshold="4" class="pb140">
       <van-tab title="核查结果">
         <platform-list :list="resultList">
           <template #fixed="{slotProps}">
-            <van-cell title="产品单元" :value="slotProps.unitname" />
-            <van-cell title="产品品种" :value="slotProps.kind" />
-            <van-cell title="规格型号" :value="slotProps.cellmodel" />
-            <van-cell title="生产地址" :value="slotProps.addrs" />
-            <van-cell title="实地核查结果">{{slotProps.ispass|dictFormatter("ispassList")}}</van-cell>
-            <van-cell title="发证检验结果">{{slotProps.testresult|dictFormatter("ispassList")}}</van-cell>
-            <van-cell title="技术审查意见">{{slotProps.ischeckfilepass|dictFormatter("ischeckfilepassList")}}</van-cell>
-            <van-cell title="抽查复查结论">{{slotProps.isccfspass|dictFormatter("isccfspassList")}}</van-cell>
-            <van-cell title="许可建议">{{slotProps.isxukepass|dictFormatter("isxukepassList")}}</van-cell>
-            <van-cell title="不予行政许可理由" value="尚未提供字段" />
+            <div class="mt10">
+              <van-cell title="产品单元" :value="slotProps.unitname" />
+              <van-cell title="产品品种" :value="slotProps.kind" />
+              <van-cell title="规格型号" :value="slotProps.cellmodel" />
+              <van-cell title="生产地址" :value="slotProps.addrs" />
+              <van-cell title="实地核查结果">{{slotProps.ispass|dictFormatter("ispassList")}}</van-cell>
+              <van-cell title="发证检验结果">{{slotProps.testresult|dictFormatter("ispassList")}}</van-cell>
+              <van-cell title="技术审查意见">{{slotProps.ischeckfilepass|dictFormatter("ischeckfilepassList")}}</van-cell>
+              <van-cell title="抽查复查结论">{{slotProps.isccfspass|dictFormatter("isccfspassList")}}</van-cell>
+              <van-cell title="许可建议">{{slotProps.isxukepass|dictFormatter("isxukepassList")}}</van-cell>
+              <van-cell title="不予行政许可理由" value="尚未提供字段" />
+            </div>
           </template>
         </platform-list>
       </van-tab>
       <van-tab title="拟许可范围">
-        <van-cell title="单位名称" :value="permitData.orgname" />
-        <van-cell title="产品名称" :value="permitData.producttype" />
-        <van-cell title="住所" :value="permitData.abode" />
-        <van-cell title="生产地址" :value="permitData.prodaddrxx" />
-        <van-cell title="拟许可范围" :value="permitData.certinfo" />
-        <van-notice-bar wrapable :scrollable="false" text="备注：拟许可范围内容如果需要修改，请同步修改“技术审查确认信息”中的相关数据" />
+        <div class="mt10">
+          <van-cell title="单位名称" :value="permitData.orgname" />
+          <van-cell title="产品名称" :value="permitData.producttype" />
+          <van-cell title="住所" :value="permitData.abode" />
+          <van-cell title="生产地址" :value="permitData.prodaddrxx" />
+          <van-cell title="拟许可范围" :value="permitData.certinfo" />
+          <van-notice-bar wrapable :scrollable="false" text="备注：拟许可范围内容如果需要修改，请同步修改“技术审查确认信息”中的相关数据" />
+        </div>
       </van-tab>
       <van-tab title="实地核查结论">
-        <van-cell title="检核开始时间" :value="conclusionData.plansdate" />
-        <van-cell title="检核结束时间" :value="conclusionData.planedate" />
-        <van-cell title="实际开始时间" :value="conclusionData.realplansdate" />
-        <van-cell title="实际结束时间" :value="conclusionData.realplanedate" />
-        <van-cell title="实地核查记录备用说明" :value="conclusionData.remark" />
-        <platform-list :list="conclusionData.prodList">
-          <template #fixed="{slotProps}">
-            <van-cell title="产品单元" :value="slotProps.unitname" />
-            <van-cell title="产品品种" :value="slotProps.kind" />
-            <van-cell title="产品规格型号" :value="slotProps.cellmodel" />
-            <van-cell title="实地核查结论">{{slotProps.ispass|dictFormatter("checkResultList")}}</van-cell>
-          </template>
-        </platform-list>
+        <div class="mt10">
+            <van-cell title="检核开始时间" :value="conclusionData.plansdate" />
+            <van-cell title="检核结束时间" :value="conclusionData.planedate" />
+            <van-cell title="实际开始时间" :value="conclusionData.realplansdate" />
+            <van-cell title="实际结束时间" :value="conclusionData.realplanedate" />
+            <van-cell title="实地核查记录备用说明" :value="conclusionData.remark" />
+            <platform-list :list="conclusionData.prodList">
+              <template #fixed="{slotProps}">
+                <van-cell title="产品单元" :value="slotProps.unitname" />
+                <van-cell title="产品品种" :value="slotProps.kind" />
+                <van-cell title="产品规格型号" :value="slotProps.cellmodel" />
+                <van-cell title="实地核查结论">{{slotProps.ispass|dictFormatter("checkResultList")}}</van-cell>
+              </template>
+            </platform-list>
+        </div>
       </van-tab>
       <van-tab title="不合格项汇总">
         <van-cell title="请选择单元">
