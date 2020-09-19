e259b68f05a6e90ce20cc637b4704ab3e3e4b479
sunwenjunnet <sunwenjunnet@sina.com>
2020-09-19 10:06:44 +0800

    提交icon样式

--- src/modules/info/ProcessInfo.vue
+++ src/modules/info/ProcessInfo.vue
@@ -8,7 +8,8 @@
             <van-row class="org-info">
               <van-col span="14" class="org-name" @click="goVerificationInfo(slotProps)">{{slotProps.orgname}}</van-col>
               <van-col span="10" class="org-tags">
-                <van-tag plain round type="primary" size="large" class="mr10">{{slotProps.flag|dictFormatter("businessType")}}</van-tag>
+                <!-- <van-tag plain round type="primary" size="large" class="mr10">{{slotProps.flag|dictFormatter("businessType")}}</van-tag> -->
+                <i :class="slotProps.stateClass"></i>
                 <van-tag plain round type="primary" size="large">{{slotProps.applydescription}}</van-tag>
               </van-col>
             </van-row>
@@ -76,6 +77,10 @@ export default {
       client.rpc("/xxgs/findGcxx",{"itemId": client.loadStorage("approvalInfo").code}).then(res=>{
         this.processTotal = "共" + res.list.length + "条";
         this.processList = res.list;
+        for(let i =0; i<res.list.length; i++){
+          let itemData =  res.list[i];
+          itemData.stateClass="stateIcon icon-stateIcon"+res.list[i].flag;
+        }
       });
   }
 };
